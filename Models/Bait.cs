d0dc3d48f6f9a174f9ffbc8901d12b499d225c5d
Salihu Hassan <hassan3130@outlook.com>
2020-12-30 09:37:44 +0100

    Added some files from Abarqu

---
+++ Models/Bait.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Abarqu.Models
+{
+    public class Bait
+    {
+        public int Id { get; set; }
+        public string ArabicText { get; set; }
+        public string EnglishText { get; set; }
+
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Bait>(this);
+        }
+    }
+}
